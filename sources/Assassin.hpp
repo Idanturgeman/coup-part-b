30d34b281a41e5db53527d1735eb008a8e544100
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-09 16:52:54 +0300

    change assassin

--- sources/Assassin.hpp
+++ sources/Assassin.hpp
@@ -1,18 +1,35 @@
 #include "Player.hpp"
-// #include "Game.hpp"
 using namespace std;
 namespace coup{
     class Assassin : public Player
     {
+    
     private:
         Player *mordechaiTraget;
+    
+    
     public:
         Assassin(Game &game, string name);
-        // int coinsduck();
-        void block(Player &p);
         void coup(Player &p);
-        void someOneBlockme();
+        void block(Player &p);
         string role();
+        void someOneBlockme();
         ~Assassin();
+    
+    
+    
+    
+    
     };
+
+
+
+
+
+
+
+
+
+
+
 }
