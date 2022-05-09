d803eae7aff12b8da499139dfcf2f870de557c7d
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-09 17:19:08 +0300

    change captain

--- sources/Captain.hpp
+++ sources/Captain.hpp
@@ -4,16 +4,20 @@ using namespace std;
 namespace coup{
     class Captain : public Player
     {
+    
+    public:
+        Captain(Game &game, string name);
+        ~Captain();
+        void steal(Player &p);
+        void someOneBlockme();
+        static void block(Player &p);
+        string role();
+        
     private:
     
     Player *pFrom;
-    public:
-        Captain(Game &game, string name);
-        static void block(Player &p);
-        void someOneBlockme();
-        void steal(Player &p);
-        string role();
-        ~Captain();
+
+
     };
 
 
