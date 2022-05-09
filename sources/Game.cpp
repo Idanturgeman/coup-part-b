eb63a5a601b043d6dc95030c3c3cc1a06859280b
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-09 19:35:24 +0300

    change game

--- sources/Game.cpp
+++ sources/Game.cpp
@@ -1,27 +1,33 @@
 #include "Game.hpp"
 namespace coup{
+    static int _players = 0;
     vector<string> Game::players()
     {
+        int i = 0;
         vector<string> names;
         for(Player *p : playersList ){
             if (p->isAlive)
             {
+                _players++;
                 names.push_back(p->name);
             }  
         }
         return names;
     }
+    static int _turn = 0;
     string Game::turn()
     {
+        
         if (this->playersList.empty())
         {
             throw runtime_error("their is no players in this game");
         }
-        // i = i % playersList.size();
+        _turn++;
         return this->playersList[i]->name;
     }
     string Game::winner()
     {
+        static int _winner = 0;
         if (this->players().size() != 1)
         {
             throw runtime_error("their is no winners in this game");
@@ -31,38 +37,48 @@ namespace coup{
             throw runtime_error("the game not start");
         }
         
-        // i = i % playersList.size();
+        _winner++;
         return this->players().at(0);
     }
     void Game::addPlayer(Player *p){
+        _players++;
         this->playersList.push_back(p);
 
         this->numOfPlayers++;
     }
     void Game::nextTurn(){
-        const int six = 6;
+        const int seven = 7;
         const int two = 2; 
+        const int six = 6;
+        const int one = 1;
+        
         do
         {
+            _turn++;
            this->i = (this->i+1) % this->playersList.size();
         } while (!this->playersList.at(i)->isAlive);
+
         if (this->numOfPlayers < two || this->numOfPlayers > six )
         {
             throw runtime_error("too much players");
         }
         if (gameStart == 0)
         {
+            _turn++;
             gameStart = 1; 
         }
     }
-    Game::Game(/* args */)
+    static int game = 0;
+    Game::Game()
     {
+        game++;
         this->i = 0;
-        this->numOfPlayers = 0;
         this->gameStart = 0;
+        this->numOfPlayers = 0;
     }
 
     Game::~Game()
     {
+        game--;
     }
 }
