8d46ac3fd354e95e966bce2147404554b409760e
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-09 19:22:19 +0300

    change duke

--- sources/Duke.hpp
+++ sources/Duke.hpp
@@ -1,17 +1,13 @@
 #include "Player.hpp"
-// #include "Game.hpp"
 using namespace std;
 namespace coup{
     class Duke : public Player
     {
-    private:
-        /* data */
     public:
         Duke(Game &game, string name);
-        // int coinsduck();
-        static void block(Player &p);
-        string role();
-        void tax();
         ~Duke();
+        void tax();
+        string role();
+        static void block(Player &p);
     };
 }
