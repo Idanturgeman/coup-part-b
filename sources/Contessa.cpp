562e3c73e44bd45c91931cf1f06a18eda6a1fb69
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-09 19:10:25 +0300

    change contessa

--- sources/Contessa.cpp
+++ sources/Contessa.cpp
@@ -1,16 +1,21 @@
 #include "Contessa.hpp"
 namespace coup{
+    static int numOfContessa = 0;
     Contessa::Contessa(Game &game, string name) : Player(game, move(name))
     {
+        numOfContessa++;
     }
     
     Contessa::~Contessa()
     {
+        numOfContessa--;
     }
     void Contessa::block(Player &p){
+        static int block = 0;
         int k = p.lastAction.compare("kill");
         if ( k == 0)
         {
+            block++;
             p.someOneBlockme();
         }
         else{
@@ -18,6 +23,8 @@ namespace coup{
         }
     }
     string Contessa::role(){
+        static int role = 0;
         return "Contessa";
+        role++;
     }
 }
