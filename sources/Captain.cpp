1f45d7637d80955f6d8f36fc2a2b3bd66b6e7803
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-09 17:03:53 +0300

    change captain

--- sources/Captain.cpp
+++ sources/Captain.cpp
@@ -1,18 +1,23 @@
 #include "Captain.hpp"
 namespace coup
 {
+    static int numOfCaptain = 0;
     Captain::Captain(Game &game, string name) : Player(game,move(name))
     {
+        numOfCaptain++;
         this->pFrom = NULL;
     }
 
     Captain::~Captain()
     {
+        numOfCaptain--;
     }
     void Captain::block(Player &p){
+        static int numOfBlock = 0;
         int s = p.lastAction.compare("steal");
         if (s == 0)
         {
+            numOfBlock++;
             p.someOneBlockme();
         }
         else{
@@ -21,26 +26,32 @@ namespace coup
         
     }
     void Captain::someOneBlockme(){
+        static int someOneBlockme = 0;
         int s = this->lastAction.compare("steal");
         int s1 = this->lastAction.compare("stealOne");
         int s2 = this->lastAction.compare("stealTwo");
         int f = this->lastAction.compare("foreign_aid");
+        someOneBlockme++;
         if (s2 == 0)
         {
+            someOneBlockme++;
             this->money -=2;
             this->pFrom->money += 2;
         }
         else if ( s1 == 0)
         {
+            someOneBlockme++;
             this->money -=1;
             this->pFrom->money += 1;
         }
         else if (s == 0)
         {
+            someOneBlockme++;
 
         }
         else if (f == 0)
         {
+            someOneBlockme++;
             this->money -= 2;
         }
         else{
@@ -48,9 +59,11 @@ namespace coup
         }
     }
     void Captain::steal(Player &p){
+        static int numOfSteal = 0;
         isMyTurn();
         if (p.money >= 2)
         {   
+            numOfSteal++;
             this->money += 2;
             p.money -= 2;
             this->pFrom = &p;
@@ -58,18 +71,22 @@ namespace coup
         }
         else if (p.money == 1)
         {
+            numOfSteal++;
             this->money += 1;
             p.money -= 1;
             this->pFrom = &p;
             endMyTurn("stealOne");
         }
         else{
+            numOfSteal++;
             this->pFrom = &p;
             endMyTurn("steal");
         }
     }
 
     string Captain::role(){
+        static int numOfRole = 0;
         return "Captain";
+        numOfRole++;
     }
 }
