b1860669a6673b9fb5a98be6e197a6816171356c
Idanturjeman <idan_turjeman29@walla.co.il>
2022-05-09 19:18:32 +0300

    change duke

--- sources/Duke.cpp
+++ sources/Duke.cpp
@@ -1,16 +1,21 @@
 #include "Duke.hpp"
 namespace coup
 {
+    static int numOfDuks = 0;
     Duke::Duke(Game &game, string name) : Player(game, move(name))
     {
+        numOfDuks++;
     }
 
     Duke::~Duke()
     {
+        numOfDuks--;
     }
     void Duke::block(Player &p){
+        static int numOfBlock = 0;
         int f = p.lastAction.compare("foreign_aid");
         if (f == 0){
+            numOfBlock++;
             p.someOneBlockme();
         }
         else{
@@ -18,13 +23,16 @@ namespace coup
         }
     }
     void Duke::tax(){
+        static int numOfTax = 0;
         isMyTurn();
         this->money += 3;
+        numOfTax++;
         this->endMyTurn("tax");
     }
     string Duke::role(){
+        static int numOfRole = 0;
         return "Duke";
-        
+        numOfRole++;
     }
 
 }
